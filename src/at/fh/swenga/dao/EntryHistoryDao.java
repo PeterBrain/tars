6e441168b6afd31763bedb1742963dd2d56d2995
Kevin <kazianschuetz.kevin@gmail.com>
2019-06-20 14:32:30 +0200

    added persist and merge to EntryHistoryDao

--- src/at/fh/swenga/dao/EntryHistoryDao.java
+++ src/at/fh/swenga/dao/EntryHistoryDao.java
@@ -6,12 +6,21 @@ import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Repository;
 
+import at.fh.swenga.model.EntryHistory;
+
 @Repository
 @Transactional
 public class EntryHistoryDao {
-	
+
 	@PersistenceContext
 	protected EntityManager entityManager;
-	
-	
+
+	public void persist(EntryHistory entryHistory) {
+		entityManager.persist(entryHistory);
+	}
+
+	public EntryHistory merge(EntryHistory entryHistory) {
+		return entityManager.merge(entryHistory);
+	}
+
 }
