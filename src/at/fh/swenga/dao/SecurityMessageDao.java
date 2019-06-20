af232416bc7448ab6b35815133170d9b9d2e3d6a
DiOps <nikolaus.koestinger@edu.fh-joanneum.at>
2019-06-20 15:35:12 +0200

    working on securit messages

--- src/at/fh/swenga/dao/SecurityMessageDao.java
+++ src/at/fh/swenga/dao/SecurityMessageDao.java
@@ -1,13 +1,25 @@
 package at.fh.swenga.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import at.fh.swenga.model.SecurityMessage;
+
 @Repository
 @Transactional
 public class SecurityMessageDao {
-	
+
 	protected EntityManager entityManager;
+
+	public List<SecurityMessage> getSecurityMessages() {
+		TypedQuery<SecurityMessage> typedQuery = entityManager.createQuery("SELECT s FROM SecurityMessage s",
+				SecurityMessage.class);
+		List<SecurityMessage> typedResultList = typedQuery.getResultList();
+		return typedResultList;
+	}
 }
